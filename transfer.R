c4430f130b43671b1e67c024954e81f351cceaff
bbest <bbest@nceas.ucsb.edu>
2014-06-19 16:01:07 -0700

    added Antarctica but getting error when running and warnings with CheckLayers():
    ```
    Scenario: Antarctica2013.a2014 -> antarctica2014
    
    Google spreadsheet editable URL:
      https://docs.google.com/spreadsheet/ccc?key=0ArcIhYsFwBeNdHNxNk1iRHc1S05KLWsyb0ZtZjRjZnc
    
    Error in gsub(lyrs$layer_old[i], lyrs$layer[i], s, fixed = T) :
      input string 8 is invalid UTF-8
    
    Warning messages in CheckLayers():
     1: Unused fields...
        eco: sector
        hab_extent: habitat
        hab_health: habitat
        hab_trend: habitat
        le_sector_weight: sector
        rgn_area: ccamlr_id,sp_id,sp_name
        rgn_global: ccamlr_id2,ccamlr_id,sp_id,sp_key,rgn_name,rgn_key
    
      2: Rows duplicated...
        eco: 64
        hab_extent: 19
        hab_health: 19
        hab_trend: 19
        le_sector_weight: 19
    ```

--- transfer.R
+++ transfer.R
@@ -1,5 +1,31 @@
 # transfer global scenarios out of ohicore
 
+# TODO NEXT...
+# Scenario: Antarctica2013.a2014 -> antarctica2014
+# 
+# Google spreadsheet editable URL:
+#   https://docs.google.com/spreadsheet/ccc?key=0ArcIhYsFwBeNdHNxNk1iRHc1S05KLWsyb0ZtZjRjZnc
+# 
+# Error in gsub(lyrs$layer_old[i], lyrs$layer[i], s, fixed = T) : 
+#   input string 8 is invalid UTF-8
+# In addition: Warning messages:
+# 1: In CheckLayers(sprintf("%s/layers.csv", scenario_new), sprintf("%s/layers",  :
+#   Unused fields...
+#     eco: sector
+#     hab_extent: habitat
+#     hab_health: habitat
+#     hab_trend: habitat
+#     le_sector_weight: sector
+#     rgn_area: ccamlr_id,sp_id,sp_name
+#     rgn_global: ccamlr_id2,ccamlr_id,sp_id,sp_key,rgn_name,rgn_key
+# 2: In CheckLayers(sprintf("%s/layers.csv", scenario_new), sprintf("%s/layers",  :
+#   Rows duplicated...
+#     eco: 64
+#     hab_extent: 19
+#     hab_health: 19
+#     hab_trend: 19
+#     le_sector_weight: 19
+
 # get paths based on host machine
 dirs = list(
   neptune_data  = '/Volumes/data_edit', 
@@ -15,29 +41,32 @@ load_all(dirs$ohicore)
 scenarios = list(
   eez2013     = list(
     name_old     = 'Global2013.www2013',    
-    google_doc   = 'https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE',
+    google_key   = '0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE',
     fld_dir      = 'dir_2013a',
     fld_fn       = 'fn_2013a',
     f_spatial    = c('../ohicore/inst/extdata/spatial.www2013/regions_gcs.js')),
   eez2012     = list(
     name_old     = 'Global2012.www2013',
-    google_doc   = 'https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE',
+    google_key   = '0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE',
     fld_dir      = 'dir_2012a',
     fld_fn       = 'fn_2012a',
     f_spatial    = c('../ohicore/inst/extdata/spatial.www2013/regions_gcs.js')),
   antarctica2014 = list(
     name_old     = 'Antarctica2013.a2014',
-    google_doc   = 'https://docs.google.com/spreadsheet/pub?key=0ArcIhYsFwBeNdHNxNk1iRHc1S05KLWsyb0ZtZjRjZnc',
-    fld_dir      = 'dir_2012a',
-    fld_fn       = 'fn_2012a'),
+    google_key   = '0ArcIhYsFwBeNdHNxNk1iRHc1S05KLWsyb0ZtZjRjZnc',
+    fld_dir      = 'dir_2013a',
+    fld_fn       = 'fn_2013a'),
   highseas2014   = list(
     name_old     = 'HighSeas2013.a2014',
-    google_doc   = 'https://docs.google.com/spreadsheet/pub?key=0ArcIhYsFwBeNdG9KVlJ6M0ZxV1dtVDJDQ3FLVWJQWFE',
+    google_key   = '0ArcIhYsFwBeNdG9KVlJ6M0ZxV1dtVDJDQ3FLVWJQWFE',
     fld_dir      = 'dir_2012a',
     fld_fn       = 'fn_2012a'))
 
-#for (i in 1:length(scenarios)){ # i=1
-for (i in 2:length(scenarios)){ # i=1
+# read only: https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE
+# editable:  https://docs.google.com/spreadsheet/ccc?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE
+
+for (i in 1:length(scenarios)){ # i=1
+#for (i in 1){ # i=3
   
   # vars
   scenario_old = scenarios[[i]][['name_old']]
@@ -46,14 +75,15 @@ for (i in 2:length(scenarios)){ # i=1
   fld_fn       = scenarios[[i]][['fld_fn']]
   dir_conf_in  = sprintf('%s/inst/extdata/conf.%s'  , dirs$ohicore, scenario_old)
   #dir_lyrs_in  = sprintf('%s/inst/extdata/layers.%s', dirs$ohicore, scenario_old)
-  cat(sprintf('Scenario: %s -> %s', scenario_old, scenario_new))
+  cat(sprintf('\nScenario: %s -> %s\n', scenario_old, scenario_new))
   
   # create dirs
   dir.create(scenario_new, showWarnings=F)
   for (dir in c('tmp','layers','conf','spatial')) dir.create(file.path(scenario_new, dir), showWarnings=F)
 
   # load Google spreadsheet for copying layers
-  g.url = sprintf('%s&output=csv', scenarios[[i]][['google_doc']])
+  cat(sprintf('\n  Google spreadsheet editable URL:\n    https://docs.google.com/spreadsheet/ccc?key=%s\n', scenarios[[i]][['google_key']]) )
+  g.url = sprintf('https://docs.google.com/spreadsheet/pub?key=%s&output=csv', scenarios[[i]][['google_key']])
   g0 = read.csv(textConnection(RCurl::getURL(g.url, ssl.verifypeer = FALSE)), skip=1, na.strings='')
   write.csv(g0, sprintf('%s/tmp/layers_0-google.csv', scenario_new), na='', row.names=F)
 
@@ -150,3 +180,6 @@ for (i in 2:length(scenarios)){ # i=1
   # launch on Mac
   system(sprintf('open %s/launchApp.command', scenario_new))
 }
+
+# create global scenario to combine the scores
+dir.create('global2014', showWarnings=F)
