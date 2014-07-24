2adb408844a024ccbec325721007232056046cf2
jules32 <jstewart@nceas.ucsb.edu>
2014-07-24 14:37:59 -0700

    formatting comments

--- global2014/merge_scores.R
+++ global2014/merge_scores.R
@@ -15,14 +15,14 @@ write.csv(d, 'scores.csv', row.names=F, na='')
 # output file for Radical...
 
 # a = read.csv('../antarctica2014/scores.csv')
-# > filter(a, region_id %in% c(0,248100) & goal=='BD') %>% arrange(region_id, goal, dimension)
-#   goal  dimension region_id score
-# 1   BD     future         0 95.87
-# 2   BD      score         0 93.63
-# 3   BD     status         0 91.39
-# 4   BD     future    248100 96.24
-# 5   BD  pressures    248100    NA
-# 6   BD resilience    248100    NA
-# 7   BD      score    248100 92.83
-# 8   BD     status    248100 89.43
-# 9   BD      trend    248100  0.01
+# filter(a, region_id %in% c(0,248100) & goal=='BD') %>% arrange(region_id, goal, dimension)
+# #   goal  dimension region_id score
+# # 1   BD     future         0 95.87
+# # 2   BD      score         0 93.63
+# # 3   BD     status         0 91.39
+# # 4   BD     future    248100 96.24
+# # 5   BD  pressures    248100    NA
+# # 6   BD resilience    248100    NA
+# # 7   BD      score    248100 92.83
+# # 8   BD     status    248100 89.43
+# # 9   BD      trend    248100  0.01
