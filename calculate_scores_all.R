65804295afae606a893d841f35b081c3acc28814
Melsteroni <frazier@nceas.ucsb.edu>
2014-08-27 08:55:44 -0700

    Last run of toolbox.

--- calculate_scores_all.R
+++ calculate_scores_all.R
@@ -35,7 +35,7 @@ scenarios = list(
     fld_dir      = 'dir_2014a',
     fld_fn       = 'fn_2014a',
     f_spatial    = c('../ohiprep/Global/NCEAS-Regions_v2014/data/regions_gcs.js'),
-    do           = T),
+    do           = F),
   eez2013     = list(
     google_key   = '0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE',
     fld_dir      = 'dir_2013a',
@@ -71,7 +71,7 @@ for (dir in c('eez2012','eez2014')){
 #   unlink(list.files(file.path(dir, 'reports/debug'), '^np_.*', full.names=T))
 # }
 
-for (i in 1:length(scenarios)){ # i=5
+for (i in 1:length(scenarios)){ # i=2
   
   # vars
   scenario   = names(scenarios)[[i]]
