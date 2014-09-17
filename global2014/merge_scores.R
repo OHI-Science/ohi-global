a716635551a8d5215f1d083d561586c8c9807fa8
Melsteroni <frazier@nceas.ucsb.edu>
2014-09-17 13:57:47 -0700

    Correcting issues with Radical file (forgot to cut out fao summaries)

--- global2014/merge_scores.R
+++ global2014/merge_scores.R
@@ -104,13 +104,21 @@ RegionalGoalScores_noAnt[RegionalGoalScores_noAnt == "eez"] <- "eez_noAntarctica
 # Regional Index scores - no Antarctica data included
 RegionalIndexScores_noAnt <- d_noAnt %>%
   filter(dimension %in% c("future", "score"),
-         goal %in% 'Index') %>%
+         goal %in% 'Index',
+         rgn_type != "fao") %>%
   group_by(rgn_type, dimension) %>%
   summarize(score = round(weighted.mean(score, area_km2, na.rm=TRUE), 2)) %>%  
   ungroup() %>%
   mutate(goal="Index", region_id=0, area_km2=NA) %>%
   select(goal, dimension, region_id, score, rgn_type, area_km2)
 RegionalIndexScores_noAnt[RegionalIndexScores_noAnt == "eez"] <- "eez_noAntarctica"
+
+## troubleshooting:
+scores <- rbind(d, GlobalGoalScores, RegionalGoalScores, RegionalIndexScores, GlobalIndexScores)
+
+scores%>%
+  filter(goal == "AO",
+         region_id == 0)
 
 scores <- rbind(d, GlobalGoalScores, RegionalGoalScores, RegionalGoalScores_noAnt, RegionalIndexScores, RegionalIndexScores_noAnt, GlobalIndexScores)
 scores <- scores %>%
@@ -129,7 +137,14 @@ write.csv(scores, file.path('global2014', sprintf('scores_2014_sameSep9data_eezN
 dir_og = '../ohi-global'
 
 s2014 <- read.csv('global2014/scores_2014_sameSep9data_eezNoAnt_2014-09-16.csv')
-s2014[s2014$region_type=="fao" & s2014$goal=="FIS",]
+
+s2014[s2014$goal=="Index" & s2014$region_type=="fao", ]
+s2014 %>%
+  filter(scenario=="2014",
+         goal == "AO",
+         region_id == 0)
+
+
 
 # ## remove eez/fao summaries (include only the global summaries)
 # s2014 <- s2014 %>%
@@ -140,7 +155,7 @@ s2012 <- read.csv(file.path(dir_og, 'eez2012/scores.csv')) %>%
 s2012$region_type[s2012$region_id==0] <- "eez_noAntarctica"
 
 s2013 <- read.csv(file.path(dir_og, 'eez2013/scores.csv')) %>%
-  mutate(scenario = 2013, region_type="eez") %>%
+  mutate(scenario = 2013, region_type="eez")
 s2013$region_type[s2013$region_id==0] <- "eez_noAntarctica"  
 
 radical <- s2013 %>%
@@ -150,8 +165,22 @@ radical <- s2013 %>%
   mutate(dimension=revalue(dimension, c('future'="likely_future_state"))) %>%
   mutate(value=round(value, 2)) 
 
+## pull these out because they don't get expanded:
 rad_region_0 <- radical[radical$region_id==0,]
+table(rad_region_0$goal, rad_region_0$dimension, rad_region_0$region_type)
+##missing a few categories (total hack...figure this out):
+radical_grid_region_0 <- expand.grid(scenario="2014",
+                                     goal=c("AO", "CP", "CS", "CW", "ECO", "HAB", "LE", "LIV", "LSP", "MAR", "NP", "TR"),
+                                     dimension=c('likely_future_state', 'score', 'status'),
+                                     region_id=0,
+                                     region_type="fao",
+                                     value=NA)
+rad_region_0 <- rbind(rad_region_0, radical_grid_region_0)
+table(rad_region_0$goal, rad_region_0$dimension, rad_region_0$region_type)
+
 rad_region_Index <- radical[(radical$goal=="Index" & radical$region_id!=0),]
+table(rad_region_Index$goal, rad_region_Index$dimension, rad_region_Index$region_type)
+
 
 radical_simple <- radical %>%
   filter(region_id != 0) %>%
@@ -175,33 +204,52 @@ radical_grid_fao <- expand.grid(scenario=c(2012,2013,2014),
 radical_grid <- rbind(radical_grid_eez, radical_grid_fao)
 
 radical_full <- merge(radical_grid, radical_simple, by=c('scenario', "goal", "dimension", "region_type", 'region_id'), all.x=TRUE)
-table(radical_full$goal[radical_full$region_type=="eez"])
-table(radical_full$goal[radical_full$region_type=="fao"])
+table(radical_full$goal[radical_full$region_type=="eez"]) #these should all be the same
+table(radical_full$goal[radical_full$region_type=="fao"]) #these should all be the same
 
 
 radical_full <- rbind(radical_full, rad_region_0, rad_region_Index)
-table(radical_full$goal[radical_full$region_type=="eez"])
+table(radical_full$goal[radical_full$region_type=="eez"]) #nothing is added to eez's because they are all summaries of no/withAntarctica
 table(radical_full$goal[radical_full$region_type=="fao"])
-radical_full[radical_full$goal=="Index" & radical_full$region_type=="fao",]
+table(rad_region_Index$region_type)
+table(rad_region_0$region_type)
 
-##missing a few categories (total hack...figure this out):
-radical_grid_fao_rgn0 <- expand.grid(scenario="2014",
-                                     goal=c("AO", "CP", "CS", "CW", "ECO", "HAB", "LE", "LIV", "LSP", "MAR", "NP", "TR"),
-                                     dimension=c('likely_future_state', 'score', 'status'),
-                                     region_id=0,
-                                     region_type="fao",
-                                     value=NA)
-
-radical_full <- rbind(radical_full, radical_grid_fao_rgn0)
-table(radical_full$goal[radical_full$region_type=="eez"])
-table(radical_full$goal[radical_full$region_type=="fao"])
-
+radical_full%>%
+  filter(scenario=="2014",
+         goal == "AO",
+         region_id == 0)
 
 radical_full  <-  radical_full %>%
   arrange(scenario, goal, dimension, region_id)
 
+radical_full[radical_full$region_type == 'eez_noAntarctica', ]
+table(radical_full$region_type)
 
-write.csv(radical_full, file.path("global2014", sprintf("/OHI_results_for_Radical_%s_full.csv", format(Sys.Date(), '%Y-%m-%d'))), row.names=F, na='')
+radical_full[radical_full$goal=="Index" & radical_full$region_id==0,]
+
+#write.csv(radical_full, file.path("global2014", sprintf("/OHI_results_for_Radical_%s_full.csv", format(Sys.Date(), '%Y-%m-%d'))), row.names=F, na='')
+
+
+radical_full <- radical_full %>%
+   filter(!(region_type %in% "eez_Antarctica")) %>%
+  filter(!(region_type %in% "fao" & region_id == 0)) #cut eez summaries that include Antarctica to be comparable across years
+
+radical_full[radical_full$region_id == 0, ]
+
+radical_full$region_id[radical_full$region_type=="eez_noAntarctica"] <- 300 #eez values get an ID of 300.
+radical_full[radical_full$region_type=="eez_noAntarctica", ]
+
+radical_full  <- radical_full %>%
+  select(-region_type)                         
+
+   
+write.csv(radical_full, file.path("global2014", sprintf("/OHI_results_for_Radical_%s_full_v3.csv", format(Sys.Date(), '%Y-%m-%d'))), row.names=F, na='')
+
+
+
+
+
+
 
 
 ## scores without Antarctica----
