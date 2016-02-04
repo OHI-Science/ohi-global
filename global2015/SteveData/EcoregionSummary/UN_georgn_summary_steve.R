46692e257b600fcb1c657ff069440a3e7581a5e0
Melsteroni <frazier@nceas.ucsb.edu>
2016-02-04 11:00:57 -0800

    Updates to map colors for Steve Ecoregion summary of 2015 scores

--- global2015/SteveData/EcoregionSummary/UN_georgn_summary_steve.R
+++ global2015/SteveData/EcoregionSummary/UN_georgn_summary_steve.R
@@ -44,7 +44,7 @@ data <- spread(data, goal, score_weighted_mean) %>%
   arrange(-Index) %>%
   filter(!is.na(UN_georegion))
 
-write.csv(data, 'global2015/UN_georgn_summary_steve.csv', row.names=FALSE)
+write.csv(data, 'global2015/SteveData/EcoregionSummary/UN_georgn_summary_steve.csv', row.names=FALSE)
 
 ##############################################
 ### data for global plot:
@@ -91,17 +91,25 @@ rownames(PlotData) <- PlotData$code
 
 mapCols <- 2:ncol(PlotData)
 
-palette <- colorRampPalette(brewer.pal(9, 'YlOrRd'))
+# palette <- colorRampPalette(brewer.pal(9, 'YlOrRd'))
+# palette <- palette(100)
+# MyColor <- palette[col.brks] 
+
+palette <- colorRampPalette(c("#F0731E", "#FFA028", "#F7C332", "#14AAC8", "#325AAA"))
 palette <- palette(100)
-col.brks <-  c(0, 10, 20, 40, 60, 70, 80, 85, 90, 95, 100)
-MyColor <- palette[col.brks] 
+MyColor <- palette[c(1, 8, 16, 24, 32, 45, 64, 73, 90, 100)] 
+col.brks <-  c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+#col.brks <- c(0, 25, 50, 75, 90, 100)
+
+
 ## This loop goes through the columns to be plotted and:
 ## 1. matches the data row names (rgn_id) to the rgn_id of the OHI regions
 ## 2. identifies the break points for colors from: 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
 ## 3. plots the maps and saves them to the "fig" file in the working directory 
 
 for (i in 1:length(mapCols)){ 
-  #i <- 3
+  #i <- 1
   s=mapCols[i]
   # Index map
   fld = names(PlotData)[s]
@@ -110,19 +118,20 @@ for (i in 1:length(mapCols)){
   
   fig = sprintf('global2015/SteveData/EcoregionSummary/global_map_mol_%s.png', fld)
   
-  col.brks = seq(0,100,length.out=11)
-
+  
   # ensure color ramp breaks contain all data
-  stopifnot(max(v,na.rm=T)<=max(col.brks) & min(v,na.rm=T)>=min(col.brks) & length(col.brks)==11)
+  stopifnot(max(v,na.rm=T)<=max(col.brks) & min(v,na.rm=T)>=min(col.brks))
   
   # plot map
   png(file=fig, width=1200, height=800, res=150, pointsize=18, type='cairo')
   par(oma=c(0,0,0,0),
       mar=c(1.5,1,0,1))
   plot(ocean, col='gray90', border=NA)
-  plot(rgnOHI, border=brewer.pal(10, 'RdYlBu')[cut(v, col.brks, labels=1:10, include.lowest=TRUE)], add=TRUE,
-       col=brewer.pal(10, 'RdYlBu')[cut(v, col.brks, labels=1:10, include.lowest=TRUE)])
-
+  #plot(rgnOHI, border=brewer.pal(10, 'RdYlBu')[cut(v, col.brks, labels=1:10, include.lowest=TRUE)], add=TRUE,
+  #     col=brewer.pal(10, 'RdYlBu')[cut(v, col.brks, labels=1:10, include.lowest=TRUE)])
+  plot(rgnOHI, border=MyColor[cut(v, col.brks, labels=1:10, include.lowest=TRUE)], add=TRUE,
+      col=MyColor[cut(v, col.brks, labels=1:10, include.lowest=TRUE)])
+  
   plot(land, col='gray80', border='grey75', add=TRUE)
   
   # get plotting dimensions
@@ -133,15 +142,18 @@ for (i in 1:length(mapCols)){
   
   # plot legend
   yh=0.05; x1=p[1]+px*0.2; x2=p[2]-px*0.2; y1=p[3]; y2=p[3]+py*yh
+  #ix = seq(x1, x2, length.out=10)
   ix = seq(x1, x2, length.out=10)
   ixd = diff(ix)[1]/2
   par(xpd=TRUE) # turn off clipping to plot region
   image(x = ix,
         y = c(y1, y2),
-        z = matrix(1:10), col=brewer.pal(10, 'RdYlBu'), add=T)
+        z = matrix(1:10), col=MyColor, add=TRUE)
+        #z = matrix(1:10), col=brewer.pal(10, 'RdYlBu'), add=T)
   rect(x1-ixd,y1,x2+ixd,y2, border='gray20')
   rect(x1-ixd*3,y1,x1-ixd,y2, border='gray20')
   text(x = c(x1-ixd*2, seq(x1-ixd, x2+ixd, length.out=11)),
+#  text(x = c(x1-ixd*2, seq(x1-ixd, x2+ixd, length.out=6)),
        y = y1, cex=0.6, pos=1, # adj=c(0.5,0), # , offset=0.1,
        labels=c('NA',as.character(col.brks))) 
   par(xpd=F) # turn back on clipping to plot region
