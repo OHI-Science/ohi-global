ac1abf6e833f1d6588b94d6726db1dde051d198f
Julia Stewart Lowndes <jstewart@nceas.ucsb.edu>
2016-06-01 09:28:40 -0700

    fix typo

--- ESM_270/test_code.r
+++ ESM_270/test_code.r
@@ -5,5 +5,5 @@ source('calculate_scores.r') # will ~30 seconds
 source('PlotFlowerMulti.r')
 PlotFlowerMulti(scores       = read.csv('scores.csv'), 
                 rgns_to_plot = 163, 
-                conf         = read.csv('conf/goals_orig.csv'), 
+                conf         = read.csv('conf/goals.csv'), 
                 name_fig     = 'reports/figures/flower_orig') # should pop up a figure in the Viewer pane bottom right (no worries if it looks cut off; it also saves a .png in the ohi-global/ESM_270/reports/figures/ folder).
