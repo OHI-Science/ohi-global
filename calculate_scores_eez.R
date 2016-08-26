0939ccb2af07d9dbdfc32cec4219f411ecaeaa72
Melsteroni <frazier@nceas.ucsb.edu>
2016-08-26 14:39:51 -0700

    OHI2016: GenEscapes pressure

--- calculate_scores_eez.R
+++ calculate_scores_eez.R
@@ -220,7 +220,7 @@ for (i in 1:length(scenarios)){  #i=2
 ### make a plot to compare different commits within a scenario
 
 change_plot(repo = "ohi-global", scenario="eez2015", commit="previous", 
-           fileSave="eez2015_prs_uv", save_csv=FALSE, save_png=FALSE)
+           fileSave="eez2015_prs_genEscapes", save_csv=FALSE, save_png=FALSE)
 
 source('../ohiprep/src/R/VisGlobal.R')
 # looking within a goal:
