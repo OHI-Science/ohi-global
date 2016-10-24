8b0781269e93d6b5d84c148e969590cc70a610cd
bbest <bdbest@gmail.com>
2016-10-24 15:58:23 -0700

    fix: # Sense of Place -> ## Sense of Place

--- temp-create_layers_meta_csv.R
+++ temp-create_layers_meta_csv.R
@@ -1,5 +1,12 @@
 library(tidyverse)
 
+# new: copy from eez2016 ---
+for (yr in 2012:2015){ # yr = 2012
+  file.copy('eez2016/conf/goals.Rmd', sprintf('eez%d/conf/goals.Rmd', yr), overwrite=T)
+}
+
+
+# old: fetch from each layers.csv ----
 for (yr in 2012:2016){ # yr = 2012
   sprintf('eez%d/layers.csv', yr) %>%
     read_csv() %>%
