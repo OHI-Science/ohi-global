d3be615b9db6ad4d96a98596de4f25f24507dd1f
Melsteroni <frazier@nceas.ucsb.edu>
2016-12-01 12:32:31 -0800

    adding in layer meta data

--- global2016/removing_noneez_test.R
+++ global2016/removing_noneez_test.R
@@ -8,18 +8,29 @@ head(data)
 ## eliminate:
 data <- data %>%
   filter(region_id != 0) %>%
-  filter(region_id <= 250) %>%
+  filter(region_id <= 250 | region_id==300) %>%
   filter(region_id != 213)
 
-write.csv(data, "radical_2016-11-17_no_HS-AQ_test.csv", row.names=FALSE)
+write.csv(data, "radical_2016-11-17_no_HS-AQ_test.csv", row.names=FALSE, na="")
 
 ## convert to NA
 data <- read.csv("global2016/radical_2016-11-17.csv")
 head(data)
 
-data <- data %>%
-  mutate(value = ifelse(region_id == 0, NA, value)) %>%
-  mutate(value = ifelse(region_id > 250, NA, value)) %>%
+data2 <- data %>%
+  # mutate(value = ifelse(region_id == 0, NA, value)) %>%
+  mutate(value = ifelse(region_id > 250 & region_id < 300, NA, value)) %>%
   mutate(value = ifelse(region_id == 213, NA, value))
-                        
-write.csv(data, "radical_2016-11-17_NAs_for_HS-AQ_test.csv", row.names=FALSE)
+
+compare <- data %>%
+  select(scenario, goal, dimension, region_id, old_value = value) %>%
+  left_join(data2, by=c("scenario", "goal", "dimension", "region_id")) %>%
+  mutate(diff = ifelse(old_value != value, "different", "same"))
+   
+tmp1 <- filter(compare, is.na(value) & !is.na(old_value) )             
+tmp2 <- filter(compare, !is.na(value) & is.na(old_value) )
+tmp3 <- filter(compare, value != old_value)
+tmp2 <- compare[compare$old_value != compare$value, ]
+       
+write.csv(data2, "global2016/radical_2016-11-17_blanks_for_HS-AQ_test.csv", row.names=FALSE, na = "")
+
