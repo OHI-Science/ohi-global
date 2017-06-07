a54272611b0107669b65f66132b4047ea613955b
Casey O'Hara <ohara@nceas.ucsb.edu>
2017-06-07 10:45:09 -0700

    why is shiny not finding files? adding warnings to help track log, including a getwd() to make sure the right working directory is going on.

--- global2016/shiny_global2016/ui.R
+++ global2016/shiny_global2016/ui.R
@@ -9,6 +9,10 @@ continents <- read_csv('data/georegion_labels.csv') %>%
   .$continent %>%
   unique()
 
+### create warning with date to help with log output
+warning(sprintf('\n\nExecuting shiny app: %s\n', Sys.time()))
+warning(sprintf('Current working directory: %s\n', getwd()))
+
 ui <- navbarPage(
   
   # tags$head(includeScript("google_analytics.js")),
