f39f176fcd110ea83a431cb34f56f5ae4f19246e
jules32 <jstewart@nceas.ucsb.edu>
2017-11-14 16:09:26 -0800

    in progress: getting ohi-global layers/goals updated

--- _site.R
+++ _site.R
@@ -13,43 +13,70 @@ suppressPackageStartupMessages({
   library(ohicore) # devtools::install_github('ohi-science/ohicore')
 })
 
-# brewed vars
-study_area      = "Global"
-gh_repo         = "ohi-global"
-gh_branch_data  = "draft"
-scenario_dir    = "eez2016"
-app_url         = "http://ohi-science.nceas.ucsb.edu/ohi-global"
-ohirepos_commit = "91f077d8e3f5fc08e21b04eab26c87d2050b6e9e"
-map_shrink_pct  = 5
+## brewed vars
+study_area      <- "Global"
+key             <- "ohi-global"
+dir_scenario_gh <- "https://raw.githubusercontent.com/OHI-Science/ohi-global/draft/eez"
 
-# derived vars
-dir_data        = sprintf('%s_%s', gh_repo, gh_branch_data)
-dir_scenario    = sprintf('%s/%s', dir_data, scenario_dir)
-gh_url          = sprintf('https://github.com/OHI-Science/%s.git', gh_repo)
-dir_report      = sprintf('%s/global2016/Reporting', dir_data)
+## knitr options for all webpages
+knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
-# knitr options
-knitr::opts_chunk$set(echo = F, message = F, warning = F)
+## read in variables if they exist (i.e. don't try for prep repos)
+scores_csv <- file.path(dir_scenario_gh, 'scores.csv')
+layers_csv <- file.path(dir_scenario_gh, 'layers.csv')
+conf_csv   <- file.path(dir_scenario_gh, 'conf/goals.csv')
 
-run_cmd = function(cmd){
-   system.time(system(cmd))
- }
+if (RCurl::url.exists(scores_csv)) scores <- readr::read_csv(scores_csv)
+if (RCurl::url.exists(layers_csv)) layers <- readr::read_csv(layers_csv)
+if (RCurl::url.exists(conf_csv))   weight <- readr::read_csv(conf_csv) %>%
+  select(goal, weight)
 
- # data branch: fetch existing, or clone new
-if (!file.exists(dir_data)){
+## save local copy of conf/web/goals.Rmd
+conf_goals_rmd <- file.path(dir_scenario_gh, 'conf/web/goals.Rmd')
 
-  # clone data branch, shallowly and quietly
-  run_cmd(sprintf('git clone -q --depth 1 --branch %s %s %s', gh_branch_data, gh_url, dir_data))
-
-} else {
-
-  # git fetch & overwrite
-  run_cmd(sprintf('cd %s; git fetch -q; git reset -q --hard origin/%s; git checkout -q %s; git pull -q', dir_data, gh_branch_data, gh_branch_data))
-
+if (RCurl::url.exists(conf_goals_rmd)) {
+  conf_goals <- readr::read_lines(conf_goals_rmd)
+  readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
 }
 
-# read config
-config = new.env()
-source(file.path(dir_scenario, 'conf/config.R'), config)
+## save local copy of conf/web/layers_all.Rmd
+layers_all_rmd <- file.path(dir_scenario_gh, 'conf/web/layers_all.Rmd')
+
+if (RCurl::url.exists(layers_all_rmd)) {
+  layers_all <- readr::read_lines(layers_all_rmd)
+  readr::write_lines(layers_all, path = 'layers_all.Rmd', append = FALSE)
+}
 
 
+
+# # derived vars
+# dir_data        = sprintf('%s_%s', gh_repo, gh_branch_data)
+# dir_scenario    = sprintf('%s/%s', dir_data, scenario_dir)
+# gh_url          = sprintf('https://github.com/OHI-Science/%s.git', gh_repo)
+# dir_report      = sprintf('%s/global2016/Reporting', dir_data)
+#
+# # knitr options
+# knitr::opts_chunk$set(echo = F, message = F, warning = F)
+#
+# run_cmd = function(cmd){
+#    system.time(system(cmd))
+#  }
+#
+#  # data branch: fetch existing, or clone new
+# if (!file.exists(dir_data)){
+#
+#   # clone data branch, shallowly and quietly
+#   run_cmd(sprintf('git clone -q --depth 1 --branch %s %s %s', gh_branch_data, gh_url, dir_data))
+#
+# } else {
+#
+#   # git fetch & overwrite
+#   run_cmd(sprintf('cd %s; git fetch -q; git reset -q --hard origin/%s; git checkout -q %s; git pull -q', dir_data, gh_branch_data, gh_branch_data))
+#
+# }
+#
+# # read config
+# config = new.env()
+# source(file.path(dir_scenario, 'conf/config.R'), config)
+#
+#
