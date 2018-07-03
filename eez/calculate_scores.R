04477ba3373d0e2bda1ba603046727c2862b2ba5
iwensu0313 <isu@bren.ucsb.edu>
2018-07-03 13:08:27 -0700

    Finished checking Natural Products (original commit of score calculation with np included was 4ea1b2f)

--- eez/calculate_scores.R
+++ eez/calculate_scores.R
@@ -146,10 +146,16 @@ write.csv(scores_all_years, 'scores.csv', na='', row.names=F)
 
 ### Some methods for visualizing the data
 
-ohicore::score_check(commit="previous", scenario_year=2018,
-            file_name="mar_seaweed_final", save_csv = TRUE, NA_compare = TRUE)
+ohicore::score_check(commit="3fb19f6", scenario_year=2016,
+            file_name="np_final", save_csv = TRUE, NA_compare = TRUE)
 
-compare <- read.csv("score_check/mar_seaweed_final_diff_data_2018-06-13.csv") 
+compare <- read.csv("score_check/np_final_diff_data_2018-07-03.csv") 
+
+tmp <- compare %>%
+  dplyr::filter(is.na(old_score) & !is.na(score) & goal == "NP")
+
+table(tmp$goal)
+
 tmp <- compare %>%
   dplyr::filter(change>0)
 table(tmp$goal)
