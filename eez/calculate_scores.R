faee26744b303fff9be1c76066e7330e5e083e7b
gclawson1 <clawsonsa1101@gmail.com>
2019-07-10 15:57:55 -0700

    pushing mariculture layers

--- eez/calculate_scores.R
+++ eez/calculate_scores.R
@@ -155,7 +155,7 @@ write.csv(scores_all_years, here('eez/scores.csv'), na='', row.names=F)
 
 # Link being sourced here is incorrect, need to change it!
 ohicore::score_check(commit="previous", scenario_year=2018,
-            file_name="mar_popn", save_csv = TRUE, NA_compare = TRUE)
+            file_name="mar_production", save_csv = TRUE, NA_compare = TRUE)
 
 
 compare <- read.csv(here("eez/score_check/wgi_res_diff_data_2019-04-15.csv")) 
