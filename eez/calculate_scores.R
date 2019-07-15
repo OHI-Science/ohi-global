dbd4943178311c64fc0670ad2f8415f223ac94db
Melsteroni <frazier@nceas.ucsb.edu>
2019-07-15 12:07:38 -0700

    small update to AO need source data

--- eez/calculate_scores.R
+++ eez/calculate_scores.R
@@ -155,12 +155,12 @@ write.csv(scores_all_years, here('eez/scores.csv'), na='', row.names=F)
 
 # Link being sourced here is incorrect, need to change it!
 ohicore::score_check(commit="previous", scenario_year=2018,
-            file_name="ao_need", save_csv = TRUE, NA_compare = TRUE)
+            file_name="ao_need_mel", save_csv = TRUE, NA_compare = TRUE)
 
 
-compare <- read.csv(here("eez/score_check/wgi_res_diff_data_2019-04-15.csv")) 
+compare <- read.csv(here("eez/score_check/mar_popn_diff_data_2019-07-10.csv")) 
 
-dplyr::filter(compare, is.na(score) & !is.na(old_score))
+tmp <- dplyr::filter(compare, !is.na(score) & is.na(old_score))
 
 library(ggplot2)
 p <- ggplot(dplyr::filter(compare, year==2018 & dimension=="status" & goal == "FIS"), aes(x=old_score, y=score)) +
