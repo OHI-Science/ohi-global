44aeabbf238ab6a084e6eb57f9cf5ffed1b4055b
gclawson1 <clawsonsa1101@gmail.com>
2020-09-04 10:56:04 -0700

    update genetic escapes data

--- eez/calculate_scores.R
+++ eez/calculate_scores.R
@@ -155,7 +155,7 @@ write.csv(scores_all_years, here('eez/scores.csv'), na='', row.names=F)
 ## final commit from last year: a832c5a
 # Link being sourced here is incorrect, need to change it!
  ohicore::score_check(commit = "previous", scenario_year = 2020,
-             file_name = "mar_SFW_sustainability_update", save_csv = TRUE, NA_compare = TRUE)
+             file_name = "mar_SFW_escapes_update", save_csv = TRUE, NA_compare = TRUE)
 
 
 compare <- read.csv(here("eez/score_check/np_new_methods_diff_data_2020-07-24.csv"))
